cffe25490c03fbc1836e5fc3954183d0ce246bfe
Bill <wkernan@gmail.com>
2016-08-09 00:28:21 -0500

    user conditions

--- config/auth.js
+++ config/auth.js
@@ -1,9 +1,9 @@
 module.exports = {
 
     'google' : {
-        'clientID'      : '970988259539-mj2ah3iroumf23oel7pgteehrudu7ear.apps.googleusercontent.com',
-        'clientSecret'  : 'Om74Qs-A63X4k_QY4OUUAUIc',
-        'callbackURL'   : 'http://localhost:3000/oauth2callback'
+        'clientID'      : process.env.clientID,
+        'clientSecret'  : process.env.clientSecret,
+        'callbackURL'   : process.env.callbackURL
     }
 
 };
