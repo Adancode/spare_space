555414f9b7ebf4357a28e58560d8365147a74c7f
Bill <wkernan@gmail.com>
2016-08-08 21:18:03 -0500

    removed comments

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,5 +1,4 @@
 var express = require('express');
-//var router = express.Router();
 var db = require('../models');
 
 module.exports = function(app, passport) {
@@ -8,28 +7,8 @@ module.exports = function(app, passport) {
 	    res.render('home');
 	});
 
-// router.post('/burgers/create', function(req, res) {
-// 	db.burger.findOne({where: {burger_name: req.body.name}}).then(function(burger) {
-// 		if(!burger) {
-// 			var aBurger;
-// 			db.burger.create({ burger_name: req.body.name, ingredient: { name: req.body.ing_name } }, { include: [db.ingredient]})
-// 			.then(function(burger) {
-// 				aBurger = burger;
-// 			})
-// 			.then(function() {
-// 				req.flash('success', 'Your ' + req.body.name + ' with ' + req.body.ing_name + ' is ready to be devoured!');
-// 				res.redirect('/burgers');
-// 			});				
-// 		} else {
-// 			req.flash('failure', 'That burger has already been made');
-// 			res.redirect('/burgers');
-// 		}
-// 	})
-// });
-
-	app.post('/spaces/create', isLoggedIn, function(req, res) {
+	app.post('/spaces/create', function(req, res) {
 		var user = req.user;
-		//db.sequelize.sync({force:true}).then(function() {
 			db.Space.create({address: req.body.address, city: req.body.city, state: req.body.state, zipcode: req.body.zipcode, description: req.body.description, price: req.body.price, from: req.body.from, to: req.body.to, photo: req.body.photo})
 			.then(function(space) {
 				user.addSpace(space);
@@ -37,7 +16,6 @@ module.exports = function(app, passport) {
 				req.flash('success', 'Your space at ' + req.body.address + ' has been created');
 				res.redirect('/spaces');
 			})
-		//})
 	})
 
 	app.get('/spaces', isLoggedIn, function(req, res){
@@ -46,15 +24,9 @@ module.exports = function(app, passport) {
 		}).then(function(data) {
 			res.render('spaces', {spaces: data});
 		});
-		// db.Space.findAll({
-
-		// }).then(function (data) {
-		// 	res.render('spaces', {spaces: data, message: req.flash()});
-		// });
 	});
 
 	app.get('/profile', isLoggedIn, function(req, res){
-		//console.log(req.user);
 		res.render('profile', {
 			user: req.user
 		});
@@ -76,19 +48,6 @@ module.exports = function(app, passport) {
       successRedirect: '/profile',
       failureRedirect: '/' 
   }));
-
-	// router.get('/sign-up', function(req, res){
-	// 	res.render('sign-up');
-	// });
-
-	// router.post('/sign-up/create', function(req, res){
-	// 	//send account information to database then redirect back to home
-	// 	res.redirect('/');
-	// });
-
-	// router.post('/login', function(req, res){
-	// 	res.redirect('/');
-	// });
 
 	app.use(function (req, res){
 		res.redirect('/');
